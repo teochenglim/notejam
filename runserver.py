1ffe69bc84bfaa771addc8132fef84887ee4172e
Teo Cheng Lim <chenglim.teo@thunes.com>
2020-11-24 21:27:30 +0800

    adding Procfile 2

--- runserver.py
+++ runserver.py
@@ -6,8 +6,8 @@ from flask_prometheus_metrics import register_metrics
 
 if __name__ == '__main__':
 
-    # app.run()
+    app.run()
 
-    register_metrics(app, app_version="v0.1.2", app_config="development")
-    dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
-    run_simple(hostname="0.0.0.0", port=5000, application=dispatcher)
+    # register_metrics(app, app_version="v0.1.2", app_config="development")
+    # dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+    # run_simple(hostname="0.0.0.0", port=5000, application=dispatcher)
